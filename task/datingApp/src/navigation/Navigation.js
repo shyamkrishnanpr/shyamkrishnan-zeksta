54476b8a3d0714b6fbfc6a6c9eac7b65695d9582
shyamkrishnanpr <122676350+shyamkrishnanpr@users.noreply.github.com>
2024-08-02 13:56:48 +0530

    other profile screen implemented

--- task/datingApp/src/navigation/Navigation.js
+++ task/datingApp/src/navigation/Navigation.js
@@ -3,14 +3,18 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
 import Home from '../screens/Home';
+import OtherProfile from '../screens/OtherProfile';
 
 const stack = createStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer >
-      <stack.Navigator initialRouteName="/home" screenOptions={{headerShown: false}}>
+    <NavigationContainer>
+      <stack.Navigator
+        initialRouteName="/home"
+        screenOptions={{headerShown: false}}>
         <stack.Screen name="/home" component={Home} />
+        <stack.Screen name="/otherProfile" component={OtherProfile} />
       </stack.Navigator>
     </NavigationContainer>
   );
