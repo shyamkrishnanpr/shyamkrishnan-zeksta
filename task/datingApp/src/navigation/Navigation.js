95f82ad67b931b13e6afef9bb49cec982b57dcd3
shyamkrishnanpr <122676350+shyamkrishnanpr@users.noreply.github.com>
2024-08-02 12:22:03 +0530

    home page integrated

--- task/datingApp/src/navigation/Navigation.js
+++ task/datingApp/src/navigation/Navigation.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 
+import Home from '../screens/Home';
+
 const stack = createStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <stack.Navigator initialRouteName="/list"></stack.Navigator>
+    <NavigationContainer >
+      <stack.Navigator initialRouteName="/home" screenOptions={{headerShown: false}}>
+        <stack.Screen name="/home" component={Home} />
+      </stack.Navigator>
     </NavigationContainer>
   );
 };
